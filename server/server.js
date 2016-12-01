fa509aab1757cee5fa6a52aa59bb39dae95c41b8
Armando Locay <alocay@gmail.com>
2016-11-30 20:05:49 -0500

    testing pg

--- server/server.js
+++ server/server.js
@@ -68,8 +68,15 @@ pool.connect(process.env.DATABASE_URL, function(err, client, done) {
   if (err) {
     return console.error('error fetching client from pool', err);
   }
-  else {
-    console.log('got client from pool');
+
+  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
   }
 });
 
